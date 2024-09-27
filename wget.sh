c6b197d9d951451bf1f66550f79d77204a78c926
Kasie Zhang <me@kasie.cc>
2024-09-27 22:25:22 +0800

    Update wget command

--- wget.sh
+++ wget.sh
@@ -5,40 +5,40 @@
 # Download
 #------------------------------
 # HTTP
-wget url                        # Download a file
-wget -c url                     # Resume download
-wget -b url                     # Background download
-wget -q url                     # Keep quiet
-wget -N url                     # Download only newer files
+wget https://example.com                        # Download a file
+wget -c https://example.com                     # Resume download
+wget -b https://example.com                     # Background download
+wget -q https://example.com                     # Keep quiet
+wget -N https://example.com                     # Download only newer files
 # FTP
 wget --ftp-user=NAME --ftp-password=PASSWORD ftp://xxx
 # Multiple files
-wget -i url_list.txt                    # Download from url list
-wget https://x.com/images/{1..50}.jpg   # Download numbered files
+wget -i urls.txt                                # Download from url list
+wget https://example.com/images/{1..50}.jpg     # Download numbered files
 # Recursive download
-wget -r url
-wget -r -A "*.pdf" url
-wget --wait=5 -r url                    # Set a delay between downloads
+wget -r https://example.com
+wget -r -A "*.pdf" https://example.com
+wget --wait=5 -r https://example.com            # Set a delay between downloads
 # Mirror a site
-wget -mkp url
+wget -mkp https://example.com
 
 #------------------------------
 # Options
 #------------------------------
 # Speed Limit
-wget --limit-rate=200k url      # Download speed limit
+wget --limit-rate=200k https://example.com      # Download speed limit
 # Rerty Attempts
-wget -t 100 url                 # Set retry attempts
+wget -t 100 https://example.com                 # Set retry attempts
 # Directory
-wget -P /dir url
+wget -P /dir https://example.com
 # Filename
-wget -o file.txt url
+wget -o file.txt https://example.com
 # Standard Output
 wget -q -O - https://xxx.com/z.tar.gz | tar -zxvf - -C ~/xxx
 # User Agent
-wget --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36" url
+wget --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36" https://example.com
 # Cookie
-wget --load-cookies cookies.txt http://example.com/
+wget --load-cookies cookies.txt http://example.com
 # Header
 wget --header="Authorization: Bearer token" http://example.com/api/data
 # Ignore Certificate Error
