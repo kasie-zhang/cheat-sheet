dbd9ec96268d672ef301a96cc883b6f6eb7e573e
Kasie Zhang <me@kasie.cc>
2024-10-07 22:56:08 +0800

    Update neovim doc
    I have set a lot plugins, features for Neovim.
    
    It's time to start a new area - VS Code, Jetbrains IDE

--- neovim.sh
+++ neovim.sh
@@ -20,69 +20,64 @@
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 mj                              # Set local mark j
 mJ                              # Set global mark J (accessible across files)
-m,                              # Set next available alpabetical (lowercase) mark
-m;                              # Toggle the next available mark at the current line
+m,                              # Toggle the next available mark at the current line
 dmj                             # Delete mark j
-dm-                             # Delete all marks in the current line
 dm<space>                       # Delete all marks in the current buffer
 m:                              # Preview mark (work across files)
-m]                              # Move to next mark
-m[                              # Move to previous mark
-
+m]/m[                           # Move to next/previous mark
 m[0-9]                          # Add a bookmark to bookmark group[0-9]
-dm.                             # Delete the bookmark grop in current line
+dm.                             # Delete digit bookmark in current line
 dm[0-9]                         # Delete all bookmarks from bookmark group[0-9]
 m{                              # Move to next bookmark in same group (work across buffers)
 m}                              # Move to previous bookmark in same group (work across buffers)
-
 'j                            ' # Jump to the exact position of mark j
-:marks                          # List marks
-
+:marks/<leader>fm               # List marks
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Motion
+# Edit
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Basic
-h / j / k / l                   # Move left / down / up / right 
-# Word
-w                               # Start of the next word
-e                               # End of the next word
-b                               # Start of the previous word
-ge                              # End of the previous word
-# Line
-0 / <leader>a                   # Move to line head
-^                               # Move to the first non-blank character of the line
-$ / <leader>d                   # Move to the line end
-g_                              # Move to the last non-blank character of the line
-C                               # Change to the end of the line (delete and enter insert mode)
+i/I                             # Enter insert mode bofore or at the beginning of the line
+a/A                             # Append text after the cursor or at the end of the line
+x                               # Delete a character
+dw/`d$`/dG                      # Delete a word / to the end of the line / to the end of the file
+cw/C                            # Change a word / to the end of the line
 D                               # Delete to the end of the line
 Y                               # Yank the entire line
+u/Ctrl-r                        # Undo and redo
+v/V/Ctrl-v                      # Enter visual mode / Select entire line / Column selection
+.                               # Repeat the last action
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Navigation
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Basic
+h/j/k/l                         # Move left/down/up/right
+# Word
+w/b                             # Start of the next/previous word
+e/ge                            # End of the next/previous word
+# Line
+<leader>a/d                     # Move to line head/end
+^/g_                            # Move to line head/end (non-blank)
+f/F<char>                       # Move to the next/previous occurrence of a character in the line
+t/T<char>                       # Like f/F<char>, but leave one character
 # Screen
-H                               # Move to the top of the screen
-M                               # Move to the middle of the screen
-L                               # Move to the button of the screen
+H/M/L                           # Move to the top/middle/bottom of the screen
 zz                              # Center the current line in the window
+s<char>
+# Position
+Ctrl-i/o                        # Jump to the next/previous location
 # Paragraph
-{                               # Move to the previous paragraph
-}                               # Move to the next paragraph
+{/}                             # Move to the previous/next paragraph
 # Section
-[[                              # Jump to the previous section
-]]                              # Jump to the next section
+[[/]]                           # Jump to the previous/next section
 # Sentence
-(                               # Jump to the beginning of the next sentence
-)                               # Jump to the beginning of the previous sentence
+(/)                             # Jump to the beginning of the next/previous sentence
 # File
-gg                              # Go to the first line of the file
-G                               # Go to the last line of the file
+gg/G                            # Go to the first/last line of the file
 :linenumber / <number>G         # Go to the specified line number
 # Miscellaneous
 %                               # Jump to the matching paretheses or brace
-f<char>                         # Jump to the next occurrence of `char` on the current line
-F<char>                         # Jump to the previous occurrence of `char` on the current line
-t<char>                         # Jump before the next occurrence of `char`
-T<char>                         # Jump after the previous occurence of `char`
-Ctrl + o                        # Jump to the previous location
-Ctrl + i                        # Jump to the next location
 
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -100,67 +95,50 @@ Ctrl + i                        # Jump to the next location
 # 7. Seperator , . ; : + - = ~ _ * # / | \ & $
 
 # Treesitter
-# 8. Assignment(a)              - la, ra, ia, aa (left side, right side, inner, outer)
+# 8. Assignment(a)              - il, ir, ia, aa (left side, right side, inner, outer)
 # 9. Condition(c)               - ic, ac
 # 10. Function(f)               - if, af
-# 11. Call(m)                   - im, am (m means method)
+# 11. Call(k)                   - ik, ak
 # 12. Class(j)                  - ij, aj (j means java)
 # 13. Loop(o)                   - io, ao
 # 14. Parameter(x)              - ix, ax (x means parameter)
-# 15. Return(r)                 - ir, ar
+# 15. Return(e)                 - ie, ae
 # 16. Number(u)                 - iu
-
-
 
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # File Explorer (nvim-tree)
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Open / Close
+# Open
 <leader>ee                      # Toggle file explorer
 <leader>ef                      # Toogle file explorer (cursor locate on current file)
 <leader>ec                      # Collapse nvim-tree recursively
-q                               # Close nvim-tree window
 # Filter / Search
-f                               # Start filter
-F                               # Clear filter
-H                               # Toggle filter: Dotfiles
-I                               # Toggle filter: Git ignore
-M                               # Toggle filter: Bookmark
+f/F                             # Start/Clear filter
+H/I/M                           # Toggle filter: Dotfiles/Git Ignore/Bookmark
 # File 
 a                               # Create a file or directory (end with /)
-c                               # Copy file
-x                               # Cut file
-p                               # Paste file
-r                               # Rename file
-e                               # Rename basename (xxx.sh -> xxx)
-ge                              # Copy basename
-y                               # Copy filename
-Y                               # Copy relative path
-gy                              # Copy absolute path
-s                               # Open a file with horizontal split
-v                               # Open a file with vertical split
-d                               # Trash
-D                               # Delete
+c/x/p                           # Copy/Cut/Paste file
+r/e                             # Rename file/basename(xxx.sh -> xxx)
+ge/y                            # Copy basename/filename
+Y/gy                            # Copy relative/absolute path
+s/v                             # Open a file with horizontal/vertical split
+d/D                             # Trash/Delete
 <CR>                            # Open
 m                               # Toggle bookmark
 # Move
-Ctrl + j                        # Next sibling (same level)
-Ctrl + k                        # Previous sibling (same level)
-j                               # Move down
-k                               # Move up
-J                               # Last sibling
-K                               # First sibling
+Ctrl-j/k                        # Next/Previous sibling (same level)
+j/k                             # Move down/up
+J/K                             # Last/First sibling
 P                               # Parent directory
->                               # CD
-<                               # Change root to parent
+>/<                             # CD / Change root to parent
 Tab                             # Open preview / Toggle directory / !CD
-[c / ]c                         # Prev / Next git (change)
+[c/]c                           # Prev / Next git (change)
 # Show
-Ctrl + i                        # Show file info
+Ctrl-i                          # Show file info
+q                               # Quit
 ?                               # Show help
-E                               # Expand all
-C                               # Collapse all
+E/C                             # Expand/Collapse all
 R                               # Refresh
 
 
@@ -177,11 +155,61 @@ R                               # Refresh
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Search
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Search file, search text
+# Vim Search
+/pattern                        # Search forward
+?pattern                        # Search backward
+n/N                             # Repeat the last search in the same/opposite direction
+<leader>nh                      # Turn all Highlighting
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Edit
+# Telescope
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Search
+<leader>fa                      # Fuzzy find string in current buffer
+<leader>fc                      # Fuzzy find to-do comments
+<leader>fd                      # Jump to function definition
+<leader>fp                      # Peek function definition
+<leader>ff                      # Fuzzy find files in cwd
+<leader>fi                      # Fuzzy find files in cwd, respecty .gitignore
+<leader>fs                      # Find string in cwd
+<leader>fw                      # Find word under cursor in cwd
+<leader>fo                      # Fuzzy find opened files
+<leader>ft                      # Find to-do comments
+<leader>fx                      # Fuzzy find executed vim commands
+<leader>fm                      # Fuzzy find marks
+<leader>fq                      # Fuzzy find quickfix list
+<leader>fl                      # Fuzzy find local list
+<leader>fr                      # Fuzzy find registers
+<leader>fj                      # Fuzzy find jumplist
+<leader>fk                      # Fuzzy find normal keymaps
+<leader>ft                      # Fuzzy find function names, variables from treesitter
+
+
+# Actions
+Ctrl-j/k                        # Next/previous (insert mode)
+j/k                             # Next/previous (normal mode)
+H/M/L                           # Select High/Middle/Low (normal mode)
+gg/G                            # Select the first/last item (normal mode)
+Ctrl-s/v                        # Go to file as a horizontal/vertical split
+Ctrl-u/d                        # Scroll up/down in preview window
+Ctrl-c/ESC                      # Close telescope (insert/normal mode)
+TAB/Shift-TAB                   # Toggle selection and move to next/previous selection
+Ctrl-q/l                        # Send selected item to quickfixlist/localist
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# TODO Comment
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# TODO: Tasks that need to be completed
+# FIXME: Code that require fixing or improvement
+# NOTE: Additional information or context for future reference
+# XXX: Potentially problemmatic or dirty code that need attention
+# HACK: A workaround or non-standard solution
+# OPTIMIZE: Suggests areas where performance improvements can be made
+# SLOW: Flags setions of code that may be slow or inefficient
+# DEBUG: Marks sections of code used for debugging purposes
+# WIP: Working in progress
+]t                              # Jump to next todo-comment
+[t                              # Jump to previous todo-comment
 
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -192,25 +220,4 @@ R                               # Refresh
 :q                              # Close current split (without closing the buffer)
 :q!                             # Close current split and discard unsaved changes
 :only                           # Close all other splits except the current one
-leader + h/j/k/l                # Move to the left/down/up/right split
-
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Micro
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Session
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Register
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Built-In Terminal
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Language Server Protocol
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Ctrl-h/j/k/l                    # Move to the left/down/up/right split
